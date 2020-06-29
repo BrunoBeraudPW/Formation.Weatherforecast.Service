1660ddf5ed574e37f27eb19483590fb4ec03323a
beraudb <beraudbruno@aol.com>
2020-06-29 13:52:54 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Common.Hosting;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using Steeltoe.Extensions.Logging.DynamicLogger;
+
+namespace Formation.Weathercast.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .UseCloudHosting(9339)
+                .AddCloudFoundry()
+                .AddDynamicLogging();
+    }
+}
